e3d4831f7301d1ac2874150b376dcd7cef12a65f
Ari Russo <ari.russo@gmail.com>
2011-09-27 01:28:32 -0400

    find by name, comments

--- lib/inst/process_chain.rb
+++ lib/inst/process_chain.rb
@@ -11,8 +11,14 @@ module Inst
       @processors = []
     end
     
+    # run all @processors on <em>msgs</em>
     def process(msgs)
       @processors.map { |processor| processor.process([msgs].flatten) }.flatten.compact
+    end
+    
+    # find the processor with the name <em>name</em>
+    def find_by_name(name)
+      @processors.find { |process| process.name == name }
     end
           
   end
