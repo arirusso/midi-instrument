f3217d37126033a088455ff933a3a07debbec847
Ari Russo <ari.russo@gmail.com>
2011-09-28 21:07:51 -0400

    moved step to state

--- lib/diamond-engine/sequencer_state.rb
+++ lib/diamond-engine/sequencer_state.rb
@@ -9,6 +9,10 @@ module DiamondEngine
       @pointer = 0
     end
     
+    def step(length)
+      @pointer = (@pointer >= (length - 1)) ? 0 : @pointer + 1
+    end
+    
     # return to the beginning of the sequence
     def reset_pointer
       @pointer = 0
