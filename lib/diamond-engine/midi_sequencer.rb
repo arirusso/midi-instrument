8fa87b13a6a135e2e39fe93e22dcde7f5dae7523
Ari Russo <ari.russo@gmail.com>
2011-09-29 23:26:53 -0400

    refactor

--- lib/diamond-engine/midi_sequencer.rb
+++ lib/diamond-engine/midi_sequencer.rb
@@ -12,11 +12,12 @@ module DiamondEngine
     attr_reader :output_process, :state
     def_delegator :@emitter, :destinations, :midi_destinations
     def_delegator :state, :reset_pointer, :reset
-    def_delegators :clock, :join, :tempo, :tempo=
+    def_delegators :state, :running?
+    def_delegators :@clock, :join, :tempo, :tempo=
     def_delegators :@emitter, :muted?, :toggle_mute, :unmute
     alias_method :focus, :join
 
-    def initialize(tempo_or_input, options = {})
+    def initialize(tempo_or_input, options = {}, &block)
       
       @events = { 
         :before_start => [],
@@ -29,7 +30,6 @@ module DiamondEngine
         :sync_updated => []
       }   
       @mute = false      
-      @output_process = ProcessChain.new
       
       midi_clock_output = options[:midi_clock_output] || false  
       devices = [(options[:midi] || [])].flatten
@@ -38,17 +38,25 @@ module DiamondEngine
       resolution = options[:resolution] || 128
 
       @sequence = options[:sequence]
+      @output_process = ProcessChain.new
+      @clock = SequencerClock.new(tempo_or_input, resolution, clock_output_devices) { tick }
       @emitter = MIDIEmitter.new(output_devices, options)
       @state = SequencerState.new
       
       initialize_syncable(options[:sync_to], options[:sync])
       initialize_callbacks
-      @clock = SequencerClock.new(tempo_or_input, resolution, clock_output_devices) { tick }
 
+      edit(&block) unless block.nil?
     end
     
+    # stops and sends all note offs
     def quiet!
       stop
+      cleanup
+    end
+    
+    # sends all note-offs
+    def cleanup
       @emitter.quiet!
     end
        
@@ -58,17 +66,23 @@ module DiamondEngine
       @emitter.mute
     end
     
+    # open the instrument for editing
+    def edit(&block)
+      self.instance_eval(&block)
+    end
+    
     # start the clock
     def start(options = {})     
+      trap "SIGINT", proc { 
+        quiet!        
+        exit
+      }
       @events[:before_start].each(&:call)
       opts = {}
       opts[:background] = true unless options[:focus] || options[:foreground]
-      trap "SIGINT", proc { 
-        stop
-        exit
-      }
-      @clock.start(opts)
       @events[:after_start].each(&:call)
+      @state.start
+      @clock.start(opts)
       true
     end
     
@@ -76,6 +90,7 @@ module DiamondEngine
     def stop
       @events[:before_stop].each(&:call)
       @clock.stop rescue false
+      @state.stop
       emit_pending_note_offs
       @events[:after_stop].each(&:call)
       true            
@@ -113,8 +128,10 @@ module DiamondEngine
       @events[:before_tick].each { |event| event.call(@state.pointer) }
       @state.step(@sequence.length)
       @sequence.at(@state.pointer) do |msgs|
-        msgs = process_output(msgs) 
-        @emitter.emit(msgs) unless muted? || msgs.nil? || msgs.empty?
+        unless msgs.nil? || msgs.empty?
+          msgs = process_output(msgs) 
+          @emitter.emit(msgs)
+        end
         @events[:after_tick].each { |event| event.call(msgs) }
       end
     end
