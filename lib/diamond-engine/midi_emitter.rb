dc185d9bc8e3996f5fc7f6f175bf1add1aea702d
Ari Russo <ari.russo@gmail.com>
2014-09-07 21:38:55 -0400

    formatting

--- lib/diamond-engine/midi_emitter.rb
+++ lib/diamond-engine/midi_emitter.rb
@@ -1,4 +1,3 @@
-#!/usr/bin/env ruby
 module DiamondEngine
   
   class MIDIEmitter
@@ -6,40 +5,49 @@ module DiamondEngine
     attr_accessor :mute
     attr_reader :clock_destinations, :destinations
     
-    # toggle mute on this emitter
+    def initialize(devices = nil, options = {})
+      @clock_destinations = []
+      @destinations = []
+      @mute = false
+      @processors = []
+      
+      add_destinations(devices) unless devices.nil?   
+    end
+
+    # Toggle mute on this emitter
     def toggle_mute
       @mute = !@mute
     end
     
-    # is this emitter muted?
+    # Is this emitter muted?
     def muted?
       @mute
     end
     
-    # stop and send note-offs to all destinations for all notes and channels
+    # Stop and send note-offs to all destinations for all notes and channels
     def quiet!
       @destinations.each do |output|
         (0..127).each do |note| 
           (0..15).each do |channel|
-            msg = MIDIMessage::NoteOff.new(channel, note, 0) 
-            output.puts(msg.to_bytes)
+            message = MIDIMessage::NoteOff.new(channel, note, 0) 
+            output.puts(message.to_bytes)
           end
         end
       end
       true
     end
     
-    # add an output where MIDI data will be sent
+    # Add an output where MIDI data will be sent
     def add_destinations(destinations)
       destinations = [destinations].flatten.compact
       @destinations += destinations
     end
     alias_method :add_destination, :add_destinations
     
-    # remove a destination
+    # Remove a destination
     def remove_destinations(destinations)
       destinations = [destinations].flatten.compact
-      @destinations.delete_if { |d| destinations.include?(d) }
+      @destinations.delete_if { |destination| destinations.include?(destination) }
     end
     alias_method :remove_destination, :remove_destinations
     
@@ -50,31 +58,22 @@ module DiamondEngine
     end
     
     def remove_clock_destinations(destinations, clock)
-      @clock_destinations.delete_if { |d| destinations.include?(d) }
+      @clock_destinations.delete_if { |destination| destinations.include?(destination) }
       clock.remove_midi_destinations(destinations)
     end
     
-    # send MIDI messages to all destinations
-    def emit(msgs)
+    # Send MIDI messages to all destinations
+    def emit(messages)
       unless muted?
-        data = as_data([msgs].flatten)
-        @destinations.each { |o| o.puts(data) }
+        data = as_data([messages].flatten)
+        @destinations.each { |destination| destination.puts(data) }
       end
     end
-        
-    def initialize(devices = nil, options = {})
-      @clock_destinations = []
-      @destinations = []
-      @mute = false
-      @processors = []
-      
-      add_destinations(devices) unless devices.nil?   
-    end
-     
+             
     private
     
-    def as_data(msgs)
-      msgs.map { |msg| msg.to_bytes }.flatten
+    def as_data(messages)
+      messages.map(&:to_bytes).flatten
     end
           
   end
