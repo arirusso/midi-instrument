7392b2aa84bfb7663f2fbd5d5c18c6af7a2a2dad
Ari Russo <ari.russo@gmail.com>
2014-09-11 23:02:28 -0400

    working example with latest changes

--- examples/simple_sequencer.rb
+++ examples/simple_sequencer.rb
@@ -7,24 +7,16 @@ $:.unshift File.join( File.dirname( __FILE__ ), '../lib')
 
 require "diamond-engine"
 
-loop_counter = 0
 sequence = [1,2,3,4]
-
 sequencer = DiamondEngine::Sequencer.new
 
-sequencer.event.perform do |data, state| 
-  puts data
-  loop_counter += 1 if state.pointer == 3
-end
-
 clock = DiamondEngine::Clock.new(120)
-
 clock.event.tick do
   sequencer.perform(sequence) && sequencer.step(sequence)
 end
 
-sequencer.trigger.stop { |data| loop_counter == 10 }
-
+sequencer.trigger.stop { |state| state.repeat == 10 }
+sequencer.event.perform { |state, data| puts data }
 sequencer.event.stop { clock.stop }
 
 clock.start(:focus => true)
