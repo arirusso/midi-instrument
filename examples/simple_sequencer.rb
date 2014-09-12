60b57cef967a50e3a5dad5a030bdb95559cfd263
Ari Russo <ari.russo@gmail.com>
2014-09-11 22:28:12 -0400

    working example

--- examples/simple_sequencer.rb
+++ examples/simple_sequencer.rb
@@ -1,20 +1,30 @@
 #!/usr/bin/env ruby
 $:.unshift File.join( File.dirname( __FILE__ ), '../lib')
 
+# This example is a sequencer that loops through the array
+# ten times
+#
+
 require "diamond-engine"
 
+loop_counter = 0
 sequence = [1,2,3,4]
-sequencer = DiamondEngine::Sequencer.new { |data| p data }
 
-clock = DiamondEngine::Clock.new(120) do
+sequencer = DiamondEngine::Sequencer.new
+
+sequencer.event.perform do |data, state| 
+  puts data
+  loop_counter += 1 if state.pointer == 3
+end
+
+clock = DiamondEngine::Clock.new(120)
+
+clock.event.tick do
   sequencer.perform(sequence) && sequencer.step(sequence)
 end
 
-sequencer.event_trigger.stop_when do |data|
-  data == 4
-end
-sequencer.event.stop do
-  clock.stop
-end
+sequencer.trigger.stop { |data| loop_counter == 10 }
+
+sequencer.event.stop { clock.stop }
 
 clock.start(:focus => true)
