ed6ce8f7716b40a2f0ddc7cfd6398eb9f75e7590
Ari Russo <ari.russo@gmail.com>
2014-09-16 23:46:08 -0700

    comments

--- lib/midi-instrument/output.rb
+++ lib/midi-instrument/output.rb
@@ -18,7 +18,7 @@ module MIDIInstrument
     # @return [Boolean]
     def channel=(channel)
       @channel_filter = if channel.nil?
-        channel
+        nil
       else
         MIDIFX::Limit.new(:channel, channel, :name => :output_channel)
       end
@@ -28,37 +28,64 @@ module MIDIInstrument
 
     # Emit messages
     # @param [*Fixnum, *MIDIMessage::NoteOn, *MIDIMessage::NoteOff, *String] args
-    # @return [Emit]
+    # @return [Array<Fixnum>]
     def puts(*args)
-      data = [args.dup].flatten
-      messages = Message.to_messages(*data)
-      bytes = if messages.nil?
-        data # array of hex numbers
-      else
-        filter_output(messages).map(&:to_bytes).flatten
+      bytes = to_bytes(args)
+      if !@mute
+        @devices.each { |output| output.puts(*bytes) }
       end
-      @devices.map { |output| output.puts(*bytes) } if !@mute
-      messages  
+      bytes  
     end
     alias_method :<<, :puts
 
+    # Toggle muted output
+    # @return [Boolean]
     def toggle_mute
       @mute = !@mute
     end
 
+    # Mute the output
+    # @return [TrueClass]
     def mute
       @mute = true
     end
 
+    # Un-mute the output
+    # @return [FalseClass]
     def unmute
       @mute = false
     end
 
+    # Is the output muted?
+    # @return [Boolean]
     def mute?
       @mute
     end
 
+    # Replace the devices with the given devices
+    # @param [Array<UniMIDI::Outputs>] devices
+    # @return [OutputContainer]
+    def devices=(devices)
+      @devices.clear
+      @devices += devices
+      @devices
+    end
+
     private
+
+    # Convert the input to MIDI message bytes
+    # @param [Array<Fixnum>, Array<MIDIMessage>] args
+    # @return [Array<Fixnum>]
+    def to_bytes(args)
+      data = [args.dup].flatten
+      messages = Message.to_messages(*data)
+      if !messages.nil?
+        messages = filter_output(messages)
+        messages.map(&:to_bytes).flatten
+      elsif Message.bytes?(data)
+        data
+      end
+    end
 
     # Filter messages for output
     # @param [Array<MIDIMessage::ChannelMessage>] messages
