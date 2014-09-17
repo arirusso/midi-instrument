f5905c676a5cc47c8a41cce8a6293e2093c93a19
Ari Russo <ari.russo@gmail.com>
2014-09-16 21:57:45 -0700

    extract methods

--- lib/midi-instrument/device.rb
+++ lib/midi-instrument/device.rb
@@ -10,12 +10,28 @@ module MIDIInstrument
     # @return [Hash] Partitioned arrays of inputs and outputs.
     def partition(devices)
       devices = [(devices || [])].flatten
-      outputs = devices.select { |device| device.respond_to?(:puts) }
-      inputs = devices.find_all { |d| d.respond_to?(:type) && d.type == :input && d.respond_to?(:gets) }.compact
+      outputs = devices.select { |device| output?(device) }
+      inputs = devices.select { |device| input?(device) }
       {
         :input => inputs,
         :output => outputs
       }
+    end
+
+    private
+
+    # Is this device an output?
+    # @param [UniMIDI::Output, Object] device
+    # @return [Boolean]
+    def output?(device)
+      device.respond_to?(:puts)
+    end
+
+    # Is this device an input?
+    # @param [UniMIDI::Input, Object] device
+    # @return [Boolean]
+    def input?(device)
+      device.respond_to?(:type) && device.type == :input && device.respond_to?(:gets)
     end
     
   end
